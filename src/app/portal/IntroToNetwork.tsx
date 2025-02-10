2c57f6e74afc4ec0ad8edf107506caae2dca2cb0
evchip <evchip@pm.me>
2025-02-10 14:56:18 +0000

    add section IntroToNetwork

---
+++ src/app/portal/IntroToNetwork.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { Typography as MuiTypography } from "@mui/material"
+import { TypographyProps as MuiTypographyProps } from "@mui/material/Typography"
+import { styled } from "@mui/material/styles"
+
+import { isMainnet } from "@/utils"
+
+import Descriptions, { DescriptionItem } from "./Descriptions"
+
+interface TypographyProps extends MuiTypographyProps {
+  bold?: boolean
+  primary?: boolean
+}
+
+const Typography = styled(MuiTypography, {
+  shouldForwardProp: prop => prop !== "bold" && prop !== "primary",
+})<TypographyProps>(({ theme, bold, primary }) => ({
+  fontWeight: bold ? 600 : 400,
+  color: primary ? (theme as any).vars.palette.primary.main : (theme as any).vars.palette.text.primary,
+}))
+
+const IntroToNetwork = () => {
+  return (
+    <>
+      <Descriptions title={`To ${isMainnet ? "𝚝𝟷 mainnet" : "𝚝𝟷 devnet"} and back in a single bound`}>
+        <DescriptionItem>
+          <Typography fontSize="2rem">
+            𝚝𝟷 is a high-speed rollup that enables near-instant withdrawals to Ethereum. Unlike optimistic and ZK rollups, 𝚝𝟷 transactions are
+            finalized in the very next Ethereum block.
+            <br />
+            <br />
+            𝚝𝟷 is powered by a custom Reth implementation running inside a TEE, allowing it to trustfully post state updates to Ethereum every block.
+            This ensures withdrawals are completed in seconds, not minutes or hours.
+            <br />
+            <br />
+            Once on 𝚝𝟷, you can trade on{" "}
+            <a href="https://t-dex.devnet.t1protocol.com/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "underline" }}>
+              <strong>T-DEX</strong>
+            </a>
+            , our fully on-chain order book DEX.
+          </Typography>
+        </DescriptionItem>
+      </Descriptions>
+    </>
+  )
+}
+
+export default IntroToNetwork
--- src/app/portal/page.tsx
+++ src/app/portal/page.tsx
@@ -7,6 +7,7 @@ import { Box, Container, Stack, Typography } from "@mui/material"
 import { L2_NAME } from "@/constants"
 import { networkType } from "@/utils"
 
+import IntroToNetwork from "./IntroToNetwork"
 import SendFeedback from "./SendFeedback"
 import TestFlow from "./TestFlow"
 import WalletConfig from "./WalletConfig"
@@ -19,7 +20,17 @@ const Portal = () => {
     <Container>
       <Box sx={{ textAlign: "center", mt: ["6.8rem", "13.8rem"] }}>
         <Typography sx={{ fontSize: ["4rem", "7.8rem"], lineHeight: 1, fontWeight: 500 }}>{L2_NAME}</Typography>
-        <Typography sx={{ fontSize: ["2rem", "2.6rem"], mt: ["2rem", "1.4rem"] }}>Get started with our {networkType} now!</Typography>
+        <Typography sx={{ fontSize: ["2rem", "2.6rem"], mt: ["2rem", "1.4rem"] }}>
+          Experience face melting{" "}
+          <img
+            src="/imgs/rollup/jet.gif"
+            alt="jet engine GIF"
+            width="90"
+            height="90"
+            style={{ borderRadius: "0.5rem", display: "inline-block", verticalAlign: "middle" }}
+          />{" "}
+          withdrawal speeds
+        </Typography>
       </Box>
       <Stack
         direction="column"
@@ -36,6 +47,7 @@ const Portal = () => {
           },
         }}
       >
+        <IntroToNetwork></IntroToNetwork>
         <WalletConfig></WalletConfig>
         <TestFlow></TestFlow>
         <SendFeedback></SendFeedback>
