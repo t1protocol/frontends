23afd467de05f37231266a17c082ca0dd5e35641
evchip <evchip@pm.me>
2025-02-11 13:19:04 +0000

    add notion tickets to TODO comments

--- src/app/bridge/Send/Withdraw.tsx
+++ src/app/bridge/Send/Withdraw.tsx
@@ -54,6 +54,7 @@ const Withdraw = () => {
   // } = useBridgeStore()
   // TODO - remove this hardcode once we have the Bridge History API enabled
   // to show the user's transaction history
+  // https://www.notion.so/t1protocol/Create-API-for-users-to-call-relayMessageWithProof-197231194dc380a48a4dec8e9c0e65c4
   const withDrawStep = "1"
 
   // const handleChange = (e, newValue) => {
