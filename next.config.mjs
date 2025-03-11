9e7e471033d035d378a10f72c6dfdbe8cc94084c
princet1protocol <prince@t1protocol.com>
2025-03-12 02:32:04 +0800

    Feature: Enabled sentry logging (#23)
    * enable sentry
    
    * Update .env to t1 shared
    
    Signed-off-by: princet1protocol <prince@t1protocol.com>
    
    ---------
    
    Signed-off-by: princet1protocol <prince@t1protocol.com>
    Co-authored-by: Wojciech Wołoszyn <w-woloszyn@users.noreply.github.com>

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@
 import packageJson from "./package.json" with { type: "json" };
 import "./scripts/download-blog-posts.data.json.mjs"
 
-// const { withSentryConfig } = require("@sentry/nextjs")
+import { withSentryConfig } from "@sentry/nextjs"
 
 const nextConfig = {
   env: {
@@ -33,48 +33,48 @@ const nextConfig = {
     ]
   },
   
-  async redirects() {
-    return [
-      {
-        source: "/developer-nft",
-        destination: "/developer-nft/mint",
-        permanent: true,
-      },
-      // blog
-      { source: "/blog/scalingSecurity", destination: "/blog/scaling-security", permanent: true },
-      { source: "/blog/founderLetter", destination: "/blog/founder-letter", permanent: true },
-      { source: "/blog/scrollSepolia", destination: "/blog/scroll-sepolia", permanent: true },
-      { source: "/blog/contributeToScroll", destination: "/blog/contribute-to-scroll", permanent: true },
-      { source: "/blog/alphaTestnet", destination: "/blog/alpha-testnet", permanent: true },
-      { source: "/blog/releaseNote0109", destination: "/blog/release-note-0109", permanent: true },
-      { source: "/blog/proofGeneration", destination: "/blog/proof-generation", permanent: true },
-      { source: "/blog/upgradingPreAlphaTestnet", destination: "/blog/upgrading-pre-alpha-testnet", permanent: true },
-      { source: "/blog/technicalPrinciples", destination: "/blog/technical-principles", permanent: true },
-      { source: "/blog/preAlphaTestnet", destination: "/blog/pre-alpha-testnet", permanent: true },
-      { source: "/blog/visionAndValues", destination: "/blog/vision-and-values", permanent: true },
-      { source: "/blog/scrollsFreshCoat", destination: "/blog/scrolls-fresh-coat", permanent: true },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/developer-nft",
+  //       destination: "/developer-nft/mint",
+  //       permanent: true,
+  //     },
+  //     // blog
+  //     { source: "/blog/scalingSecurity", destination: "/blog/scaling-security", permanent: true },
+  //     { source: "/blog/founderLetter", destination: "/blog/founder-letter", permanent: true },
+  //     { source: "/blog/scrollSepolia", destination: "/blog/scroll-sepolia", permanent: true },
+  //     { source: "/blog/contributeToScroll", destination: "/blog/contribute-to-scroll", permanent: true },
+  //     { source: "/blog/alphaTestnet", destination: "/blog/alpha-testnet", permanent: true },
+  //     { source: "/blog/releaseNote0109", destination: "/blog/release-note-0109", permanent: true },
+  //     { source: "/blog/proofGeneration", destination: "/blog/proof-generation", permanent: true },
+  //     { source: "/blog/upgradingPreAlphaTestnet", destination: "/blog/upgrading-pre-alpha-testnet", permanent: true },
+  //     { source: "/blog/technicalPrinciples", destination: "/blog/technical-principles", permanent: true },
+  //     { source: "/blog/preAlphaTestnet", destination: "/blog/pre-alpha-testnet", permanent: true },
+  //     { source: "/blog/visionAndValues", destination: "/blog/vision-and-values", permanent: true },
+  //     { source: "/blog/scrollsFreshCoat", destination: "/blog/scrolls-fresh-coat", permanent: true },
 
-      { source: "/sticker-vote", destination: "/sticker-winners", permanent: true },
-      // { source: "/airdrop-faq", destination: "https://scroll-faqs.gitbook.io/faqs", permanent: false},
+  //     { source: "/sticker-vote", destination: "/sticker-winners", permanent: true },
+  //     // { source: "/airdrop-faq", destination: "https://scroll-faqs.gitbook.io/faqs", permanent: false},
 
-    ]
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/canvas/listing',
-        destination: 'https://badge-registry.canvas.scroll.cat/badge-registry'
-      },
-      {
-        source: '/badge-registry/:path*',
-        destination: 'https://badge-registry.canvas.scroll.cat/badge-registry/:path*'
-      },
-      {
-        source: '/gov-docs/:path*',
-        destination: 'https://scroll-governance-documentation.vercel.app/:path*'
-      }
-    ]
-  },
+  //   ]
+  // },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/canvas/listing',
+  //       destination: 'https://badge-registry.canvas.scroll.cat/badge-registry'
+  //     },
+  //     {
+  //       source: '/badge-registry/:path*',
+  //       destination: 'https://badge-registry.canvas.scroll.cat/badge-registry/:path*'
+  //     },
+  //     {
+  //       source: '/gov-docs/:path*',
+  //       destination: 'https://scroll-governance-documentation.vercel.app/:path*'
+  //     }
+  //   ]
+  // },
   images: {
     remotePatterns: [
       {
@@ -147,43 +147,39 @@ const nextConfig = {
 
     return config
   },
-  // sentry: {
-  //   // For all available options, see:
-  //   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  sentry: {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-  //   // Upload a larger set of source maps for prettier stack traces (increases build time)
-  //   widenClientFileUpload: true,
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
 
-  //   // Transpiles SDK to be compatible with IE11 (increases bundle size)
-  //   transpileClientSDK: true,
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
 
-  //   // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-  //   // tunnelRoute: "/monitoring-tunnel",
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    // tunnelRoute: "/monitoring-tunnel",
 
-  //   // Hides source maps from generated client bundles
-  //   hideSourceMaps: true,
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
 
-  //   // Automatically tree-shake Sentry logger statements to reduce bundle size
-  //   disableLogger: true,
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
 
-  //   // Enables automatic instrumentation of Vercel Cron Monitors.
-  //   // See the following for more information:
-  //   // https://docs.sentry.io/product/crons/
-  //   // https://vercel.com/docs/cron-jobs
-  //   automaticVercelMonitors: true,
-  // },
+    // Enables automatic instrumentation of Vercel Cron Monitors.
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    // automaticVercelMonitors: true,
+  },
 }
 export default nextConfig
 // Injected content via Sentry wizard below
 
-// module.exports = withSentryConfig(nextConfig, {
-//   // For all available options, see:
-//   // https://github.com/getsentry/sentry-webpack-plugin#options
-
-//   // Suppresses source map uploading logs during build
+// export default withSentryConfig(nextConfig, {
 //   silent: true,
 //   url: process.env.SENTRY_URL,
 //   org: process.env.SENTRY_ORG,
 //   project: process.env.SENTRY_PROJECT,
 //   authToken: process.env.SENTRY_AUTH_TOKEN,
-// })
+// });
